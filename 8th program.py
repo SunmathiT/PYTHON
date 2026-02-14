bba00de03480f834dd82db6cf7feee89706d42c1
SunmathiT <sunmathi0256@gmail.com>
2026-02-14 19:48:36 +0530

    Add files via upload

---
+++ 8th program.py
@@ -0,0 +1,46 @@
+import pandas as pd
+products = {
+    "ProductID": [1, 2, 3, 4, 5],
+    "ProductName": ["Rice", "Milk", "Bread", "Oil", "Sugar"],
+    "Category": ["Grocery", "Dairy", "Bakery", "Grocery", "Grocery"],
+    "CostPrice": [40, 20, 25, 120, 35]
+}
+
+df_products = pd.DataFrame(products)
+sales = {
+    "ProductID": [1, 2, 3, 4, 5],
+    "QuantitySold": [50, 120, 80, 40, 60],
+    "SellingPrice": [55, 30, 40, 150, 50]
+}
+
+df_sales = pd.DataFrame(sales)
+df = pd.merge(df_products, df_sales, on="ProductID")
+
+print("\nDATA INFORMATION")
+print(df.info())
+
+print("\nSTATISTICAL SUMMARY")
+print(df.describe())
+
+df["Profit"] = (df["SellingPrice"] - df["CostPrice"]) * df["QuantitySold"]
+df["Revenue"] = df["SellingPrice"] * df["QuantitySold"]
+print("\nSORTED BY PROFIT (DESCENDING)")
+print(df.sort_values(by="Profit", ascending=False))
+print("\nPRODUCTS WITH REVENUE GREATER THAN 4000")
+print(df[df["Revenue"] > 4000])
+print("\nGROCERY CATEGORY PRODUCTS")
+print(df[df["Category"] == "Grocery"])
+print("\nCATEGORY WISE TOTAL REVENUE")
+print(df.groupby("Category")["Revenue"].sum())
+
+print("\nCATEGORY WISE TOTAL PROFIT")
+print(df.groupby("Category")["Profit"].sum())
+
+print("\nBEST SELLING PRODUCT")
+print(df.loc[df["Revenue"].idxmax()])
+
+print("\nLEAST PROFIT PRODUCT")
+print(df.loc[df["Profit"].idxmin()])
+
+print("\nFINAL SUPERMARKET SALES REPORT")
+print(df)
